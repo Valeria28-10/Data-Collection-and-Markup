6cc5e10cba54877e7eb57ec7763ef9eb39da89fa
Valeria28-10 <valeriakonovalenko828@gmail.com>
2024-09-04 21:11:17 +0300

    result.json

---
+++ Homework_2/main.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
+ua = UserAgent() # создаем рандомный User-Agent
+print(ua.safary)
+
+# url = "https://www.boxofficemojo.com/intl/?ref_=bo_nb_hm_tab"
+# url = "https://www.boxofficemojo.com"
+
+# headers = {"User-Agent": ""}
+
